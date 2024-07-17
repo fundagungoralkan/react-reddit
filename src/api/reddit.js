b00c0ea494b519a86429dfd08fe62478fe77291f
FundaGungorAlkan <fundaalkan112@gmail.com>
2024-07-17 19:25:44 +0100

    ..

---
+++ src/api/reddit.js
@@ -0,0 +1,29 @@
+export const API_ROOT = 'https://www.reddit.com';
+
+export const getSubredditPosts = async (subreddit) => {
+    const response = await fetch(`${API_ROOT}${subreddit}.json`);
+    const json = await response.json();
+
+    return json.data.children.map((allPost) => allPost.data);
+};
+
+export const getSubreddits = async () => {
+    const response = await fetch(`${API_ROOT}/subreddits.json`);
+    const json = await response.json();
+
+    return json.data.children.map((subreddit) => subreddit.data);
+};
+
+export const getPostComments = async (permalink) => {
+    const response = await fetch(`${API_ROOT}${permalink}.json`);
+    const json = await response.json();
+  
+    return json[1].data.children.map((subreddit) => subreddit.data);
+};
+
+export const searchPosts = async (query) => {
+    const response = await fetch(`${API_ROOT}/search.json?q=${query}`);
+    const json = await response.json();
+
+    return json.data.children.map((post) => post.data);
+};
